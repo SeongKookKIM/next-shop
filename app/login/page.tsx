8bc817827eaa4782dfb5af3ee258f3e90d3bea61
SeongKookKIM <ksk8646@naver.com>
2024-01-03 11:44:11 +0900

    react-hook-form 사용 (로그인 페이지 적용)

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,24 +2,59 @@
 
 import { useRouter } from "next/navigation";
 import React from "react";
+import { useForm } from "react-hook-form";
 import { FcGoogle } from "react-icons/fc";
 import { GrGithub } from "react-icons/gr";
 
 function page() {
   const router = useRouter();
 
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, isSubmitted, errors },
+  } = useForm();
+
+  const testForm = async (data: any) => {
+    await new Promise((r) => setTimeout(r, 1000));
+    console.log(data);
+  };
+
   return (
     <div className="login">
       <div className="login-inner">
         <h1 onClick={() => router.push("/")}>SHOP</h1>
-        <form>
-          <input type="text" name="id" placeholder="아이디를 입력해주세요." />
+        <form onSubmit={handleSubmit(testForm)}>
+          <input
+            type="text"
+            placeholder="아이디를 입력해주세요."
+            aria-invalid={
+              isSubmitted ? (errors.id ? "true" : "false") : undefined
+            }
+            {...register("id", {
+              required: "* 아이디를 입력해주세요.",
+            })}
+          />
+          {errors.id && (
+            <p className="login-alert">{errors.id.message?.toString()}</p>
+          )}
           <input
             type="password"
-            name="password"
             placeholder="비밀번호를 입력해주세요."
+            aria-invalid={
+              isSubmitted ? (errors.password ? "true" : "false") : undefined
+            }
+            {...register("password", {
+              required: "* 비밀번호를 입력해주세요.",
+            })}
           />
-          <button type="submit">로그인</button>
+          {errors.password && (
+            <p className="login-alert">{errors.password.message?.toString()}</p>
+          )}
+
+          <button type="submit" disabled={isSubmitting}>
+            로그인
+          </button>
         </form>
         <div className="login-signin">
           <span>회원가입</span>

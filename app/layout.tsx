3f291dd29b4bbd025699fbed3f5a7afee673f489
SeongKookKIM <ksk8646@naver.com>
2024-01-25 17:13:16 +0900

    body 반응형 css 설정, meta태그 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Head from "next/head";
 import { Inter } from "next/font/google";
 import { cookies } from "next/headers";
 import "./Style.scss";
@@ -21,6 +22,12 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, user-scalable=no"
+        />
+      </Head>
       <body
         className={
           cookieRes != undefined && cookieRes.value == "dark" ? "dark-mode" : ""
